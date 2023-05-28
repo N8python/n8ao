b3b6a3df431844b8f5e479c0cb1c054a086544bd
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-05-28 11:56:01 -0500

    chore: cleanup

--- src/FullScreenTriangle.js
+++ src/FullScreenTriangle.js
@@ -2,40 +2,40 @@ import * as THREE from 'three';
 
 const _geometry = new THREE.BufferGeometry();
 _geometry.setAttribute(
-	'position',
-	new THREE.BufferAttribute(new Float32Array([-1, -1, 3, -1, -1, 3]), 2)
+  'position',
+  new THREE.BufferAttribute(new Float32Array([-1, -1, 3, -1, -1, 3]), 2)
 );
 _geometry.setAttribute(
-	'uv',
-	new THREE.BufferAttribute(new Float32Array([0, 0, 2, 0, 0, 2]), 2)
+  'uv',
+  new THREE.BufferAttribute(new Float32Array([0, 0, 2, 0, 0, 2]), 2)
 );
 
 // Recent three.js versions break setDrawRange or itemSize <3 position
-_geometry.boundingSphere = new THREE.Sphere()
-_geometry.computeBoundingSphere = () => {}
+_geometry.boundingSphere = new THREE.Sphere();
+_geometry.computeBoundingSphere = function() {};
 
 const _camera = new THREE.OrthographicCamera()
 
 export class FullScreenTriangle {
-	constructor(material) {
-		this._mesh = new THREE.Mesh(_geometry, material);
-		this._mesh.frustumCulled = false;
-	}
+  constructor(material) {
+    this._mesh = new THREE.Mesh(_geometry, material);
+    this._mesh.frustumCulled = false;
+  }
 
-	render(renderer) {
-		renderer.render(this._mesh, _camera);
-	}
+  render(renderer) {
+    renderer.render(this._mesh, _camera);
+  }
 
-	get material() {
-		return this._mesh.material;
-	}
+  get material() {
+    return this._mesh.material;
+  }
 
-	set material(value) {
-		this._mesh.material = value;
-	}
+  set material(value) {
+    this._mesh.material = value;
+  }
 
   dispose() {
-		this._mesh.material.dispose();
-		this._mesh.geometry.dispose();
-	}
+    this._mesh.material.dispose();
+    this._mesh.geometry.dispose();
+  }
 }
